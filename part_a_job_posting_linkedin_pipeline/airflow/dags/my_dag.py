08b4b8e955c55820bb291b6970a202fa77a39e38
xiangivyli <xiangivyli@gmail.com>
2024-03-28 18:09:10 +0000

    start again!

--- part_a_job_posting_linkedin_pipeline/airflow/dags/my_dag.py
+++
@@ -1,6 +0,0 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime
-
-with DAG("my_dag", start_date=datetime(2024, 3, 31),
-         schedule_interval="@daily", catchup=False) as dag:
