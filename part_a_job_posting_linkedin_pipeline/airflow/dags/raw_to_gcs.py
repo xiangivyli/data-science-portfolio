c6f5632d5a085c367407b5356c7637446eeea652
xiangivyli <xiangivyli@gmail.com>
2024-03-30 11:04:39 +0000

    combine upload and repartition together

--- part_a_job_posting_linkedin_pipeline/airflow/dags/raw_to_gcs.py
+++ part_a_job_posting_linkedin_pipeline/airflow/dags/raw_to_gcs.py
@@ -1,38 +1,61 @@
-from airflow.decorators import dag, task
-from datetime import datetime
-
+from airflow import DAG
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
-
-from pathlib import Path
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from datetime import datetime
 import glob
+from pathlib import Path
 
-
-@dag(
+# Define your DAG
+with DAG(
+    'csv_to_gcs',
     start_date=datetime(2024, 3, 29),
-    schedule=None,
+    schedule_interval=None,
     catchup=False,
     tags=['step1_csv_to_gcs'],
-)
+) as dag:
 
-def csv_to_gcs():
-    #list all CSV files
+    # List all CSV files
     csv_files = glob.glob('/usr/local/airflow/include/dataset/**/*.csv', recursive=True)
+
+    # Keep track of all the upload tasks
+    upload_tasks = []
     
-
+    # Iterate over each file and create a task for each one
     for file_path in csv_files:
-        # generate unique task id
-        task_id=f'upload_{Path(file_path).stem}_to_gcs'
+        # Generate unique task_id
+        task_id = f'upload_{Path(file_path).stem}_to_gcs'
+        
+        # Construct the destination path in GCS
+        dst_path = f'final_project/raw/{Path(file_path).name}'
 
-        #construct the destunation path in GCS
-        dst_path = 'final_project/raw/' + Path(file_path).name
-
-        #create a task to upload the files to GCS
-        upload_to_gcs = LocalFilesystemToGCSOperator(
+        # Create a task to upload the files to GCS
+        upload_task = LocalFilesystemToGCSOperator(
             task_id=task_id,
             src=file_path,
             dst=dst_path,
             bucket='de-zoomcamp-xiangivyli',
             gcp_conn_id='gcp',
-            mime_type='text/csv',)
-   
-dag_instance = csv_to_gcs()
+            mime_type='text/csv',
+        )
+        
+        upload_tasks.append(upload_task)
+
+    # Define the Spark job submission task
+    submit_job = SparkSubmitOperator(
+        task_id='submit_job',
+        application='/include/pyspark_script.py', #/path/to/my/pyspark_script.py
+        conn_id='spark_default',
+        total_executor_cores='1',
+        executor_memory='2g',
+        num_executors='1',
+        driver_memory='2g',
+        verbose=False,
+        env_vars={
+            'JAVA_HOME': '/home/xiangivyli/spark/jdk-11.0.2',
+            'SPARK_HOME': '/home/xiangivyli/spark/spark-3.3.2-bin-hadoop3'
+        }
+    )
+
+    # Set dependencies: the Spark job should run after all upload tasks are complete
+    for upload_task in upload_tasks:
+        upload_tasks >> submit_job
