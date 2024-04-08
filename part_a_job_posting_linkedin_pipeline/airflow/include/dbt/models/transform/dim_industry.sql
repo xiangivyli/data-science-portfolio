19b165d6a7b13801daa51d15531d1340b340fcbb
xiangivyli <xiangivyli@gmail.com>
2024-04-08 21:22:17 +0000

    practice to integrate dbt in airflow

---
+++ part_a_job_posting_linkedin_pipeline/airflow/include/dbt/models/transform/dim_industry.sql
@@ -0,0 +1,17 @@
+with job_industry as (
+    select *
+    from {{ source('job_postings_project', 'job_industries') }}
+),
+
+industry as (
+    select CAST (industry_id AS STRING) AS industry_id,
+           industry_name
+    from {{ source('job_postings_project', 'industries') }}
+
+)
+
+select job_industry.job_id,
+       industry.industry_name
+from job_industry
+left join industry
+on job_industry.industry_id = industry.industry_id
---
+++ part_a_job_posting_linkedin_pipeline/airflow/include/dbt/target/compiled/job_postings_project/models/transform/dim_skill.sql
@@ -0,0 +1,16 @@
+with job_skills as (
+    select *
+    from `cedar-style-412618`.`job_postings_project`.`job_skills`
+),
+
+skills_name as (
+    select *
+    from `cedar-style-412618`.`job_postings_project`.`skills`
+
+)
+
+select job_skills.job_id,
+       skills_name.skill_name
+from job_skills
+left join skills_name 
+on job_skills.skill_abr = skills_name.skill_abr
